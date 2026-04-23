71c11db60ec5bf11884e2d8c21a130308dd69bd4
cloderno <cloderno@gmail.com>
2026-04-23 17:47:52 +0500

    postrepository

---
+++ blog/src/main/java/com/cloderno/blog/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.cloderno.blog.repository;
+
+import com.cloderno.blog.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
